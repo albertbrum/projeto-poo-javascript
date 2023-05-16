1f4929c868f46ffb489a7ef7f61e479fac9553c8
albertbrum <65706837+albertbrum@users.noreply.github.com>
2023-05-16 07:32:14 -0300

    conceito de objetos e modularização

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
+import capitalizar from "./modificador";
+import ordenar from "./modificador"
+
+console.log(capitalizar);
+
 var ingredientes = ['água', 'mel', 'sal', 'mostarda'];
-var modificado = [];
-
-function capitalizar(ingredientes){
-    var modificado = [];
-
-    for (var i  = 0; i < ingredientes.length; i++){
-        var letraInicial = 
-            ingredientes[i].charAt(0).toUpperCase();
-        var restoTexto = 
-            ingredientes[i].slice(1);
-        var resultado = letraInicial + restoTexto;
-    
-        modificado[i] = resultado;
-    }
-
-    return modificado;
-}
-
-function ordenar(ingredientes){
-    return ingredientes.sort(function(a,b){
-        return a.localeCompare(b);
-    });
-}
-
 var resultadoCapitalizado = capitalizar(ingredientes);
 var resultadoOrdenacao = ordenar(resultadoCapitalizado);
 
-console.log(resultadoCapitalizado)
-console.log(resultadoOrdenacao)
-
-
-
-
-
-
+console.log(resultadoCapitalizado);
+console.log(resultadoOrdenacao);
 
 
 // // var ordenado = modificado.sort(function(a,b){
